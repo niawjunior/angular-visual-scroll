610708d5ad8ac1f43a7988f1093730dd7b2c4038
niawjunior <pasupol@dome.cloud>
2019-01-09 15:35:05 +0700

    :tada:

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers = Array.from({ length: 10000 }).map((_, index) => `Item ${index}`);
+}
